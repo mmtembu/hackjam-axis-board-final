bce6beaac54f634d57c8266dcd4874f00abad736
Mangaliso Elvynn Mtembu <mmtembu@student.wethinkcode.co.za>
2022-10-08 12:14:36 +0200

    Revert "4 register screen continued"

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hackjam_axis_board_front_end/common_components/common_components.dart';
-import 'package:hackjam_axis_board_front_end/pages/registration.dart';
-import 'package:hackjam_axis_board_front_end/pages/login.dart';
 import 'config/pallet.dart';
 
 void main() {
@@ -14,21 +10,40 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AxisBoard',
+      title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Pallet.kToDark,
       ),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'AxisBoard'),
+      home: const MyHomePage(title: 'Axis Board'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
 
@@ -52,25 +67,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     const logoSvg =
         '<svg width="34" height="28" viewBox="0 0 34 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.4003 2.82336C13.4003 1.32041 13.8105 0.851875 14.4564 0.425937C9.17576 0.943147 3.11254 2.49478 1.06108 4.46626C0.650784 4.93479 0.354462 5.7319 0.354462 6.72373C0.354462 13.0215 8.46914 27.26 14.3424 27.26C17.1005 27.26 21.7353 23.6395 25.4887 18.8021C24.9568 18.942 24.6073 18.942 24.0831 18.942C19.737 18.942 13.4003 6.86368 13.4003 2.82336V2.82336ZM22.7306 0C20.4436 0 19.5622 0.328581 19.5622 2.20879C19.5622 6.24911 23.65 14.2872 27.2894 14.2872C29.2877 14.2872 33.2767 8.22059 33.2767 3.19453C33.2767 0.705839 28.186 0 22.7306 0Z" fill="black"/></svg>';
-    void displaySnackBar(item) => ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content:
-                Text("Yes please check it's me, $item has been pressed !!!"),
-          ),
-        );
-
-    void navigateToPage(page) => Navigator.push(
-          context,
-          CupertinoPageRoute<bool>(
-            fullscreenDialog: false,
-            builder: (context) => page.toString().toUpperCase() == 'REGISTER'
-                ? const Registration()
-                : const Login(),
-          ),
-        );
-
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(15),
@@ -99,17 +103,67 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Align(
+              // alignment: FractionalOffset.bottomCenter,
               alignment: const Alignment(0.0, 0.85),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  submitbutton(
-                    navigateToPage,
-                    'LOGIN',
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: OutlinedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        elevation: 3.0,
+                        shadowColor: Colors.black,
+                      ),
+                      onPressed: (() => {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text(
+                                    "Yes please check it's me, Login has been pressed!!!"),
+                              ),
+                            ),
+                          }),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          'LOGIN',
+                          style: GoogleFonts.comfortaa(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  submitbutton(
-                    navigateToPage,
-                    'REGISTER',
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        elevation: 3.0,
+                        shadowColor: Colors.black,
+                      ),
+                      onPressed: (() => {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text(
+                                    "Yes please check it's me, Registration has been pressed !!!"),
+                              ),
+                            ),
+                          }),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          "REGISTER",
+                          style: GoogleFonts.comfortaa(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
                   )
                 ],
               ),
